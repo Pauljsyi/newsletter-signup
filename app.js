52bd2e5d3e2bec7eb06b3eae2e23cdfcad98e8ef
Paul Yi <pauljsyi@gmail.com>
2021-01-20 00:52:03 -0800

    failure page redirect

--- app.js
+++ app.js
@@ -4,11 +4,70 @@ const request = require('request')
 const app = express();
 const https = require('https');
 
-app.get('/', function(req, res){
-    res.send('hello world!')
+
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html")
+    
 })
+
+app.post("/", function(req, res) {
+
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us7.api.mailchimp.com/3.0/lists/db68acce84"
+
+    const options = {
+        method: "POST",
+        auth: "paul1:6a952fb11f341bd629cfeb55dfacaa20-us7"
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        } else if (response.statusCode === 404) {
+            res.sendFile(__dirname + "/failure.html")
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+   request.write(jsonData)
+   request.end()
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect('/')
+})
+
 
 app.listen(3000, function(){
     console.log('server is running on port 3000')
 })
 
+//audienceID or list_ID = db68acce84
+// var apiKey = 6a952fb11f341bd629cfeb55dfacaa20-us7
